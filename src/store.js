037731de70e5f4ed3f1e3520c3d2f61130cea7f7
Sasha <alexander.goryachev@gmail.com>
2021-04-20 14:00:59 +0300

    learn enhancer

--- src/store.js
+++ src/store.js
@@ -1,7 +1,51 @@
-import { createStore } from "redux";
+import { createStore, compose } from "redux";
 
 import reducer from "./reducers";
 
-const store = createStore(reducer);
+const strEnhancer = createStore => (...args) => {
+  const store = createStore(...args);
+  const originalDispatch = store.dispatch;
+  store.dispatch = (action) => {
+    if (typeof action === 'string') {
+      return originalDispatch({
+        type: action
+      });
+    }
+
+    return originalDispatch(action);
+  };
+
+  return store;
+};
+
+const logEnhancer = createStore => (...args) => {
+  const store = createStore(...args);
+  const originalDispatch = store.dispatch;
+  store.dispatch = (action) => {
+    console.log(action.type);
+
+    return originalDispatch(action);
+  };
+
+  return store;
+};
+
+const store = createStore(reducer, compose(strEnhancer, logEnhancer));
+
+// manky patching
+//
+// const originalDispath = store.dispatch;
+// store.dispatch = (action) => {
+//
+//   if (typeof action === 'string') {
+//     return originalDispath({
+//       type: action
+//     });
+//   }
+//
+//   return originalDispath(action);
+// }
+
+store.dispatch('HELLO_WORLD');
 
 export default store;
